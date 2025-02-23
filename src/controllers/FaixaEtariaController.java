130f66c4660a52584d63a951ae7ce9ddd40a5139
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-02-23 20:14:09 -0300

    Primeiro commit

---
+++ src/controllers/FaixaEtariaController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import application.Main;
+import javafx.fxml.FXML;
+
+public class FaixaEtariaController {
+    @FXML
+    private void trocarParaCalculoMedia() {
+        Main.mudarTela("calculo_media");
+    }
+
+}
